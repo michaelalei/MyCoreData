8f5e49e9b64291bf505beb2f03d0469e35d03bd3
qianfeng <qianfeng@001-Teacher-Zhu.local>
2014-10-08 16:05:18 +0800

    Initial Commit

---
+++ CoreData01Tests/CoreData01Tests.m
@@ -0,0 +1,34 @@
+//
+//  CoreData01Tests.m
+//  CoreData01Tests
+//
+//  Created by qianfeng on 14-10-8.
+//  Copyright (c) 2014年 Qianfeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoreData01Tests : XCTestCase
+
+@end
+
+@implementation CoreData01Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
